cbbbfbab7fc8f3582d0a9b266bd4f739303b7ae0
Chu Phong <chucamphong1999@gmail.com>
2020-03-29 01:00:59 +0700

    Sử dụng hàm hasRole thay thế cho isMember

--- src/modules/App/containers/App.tsx
+++ src/modules/App/containers/App.tsx
@@ -1,23 +1,17 @@
 import { useAuth } from "modules/Auth";
-import { User } from "modules/Auth/types";
 import DashBoardContainer from "modules/Dashboard/containers/DashBoard";
 import LoadingContainer from "modules/Loading/containers/Loading";
 import LoginContainer from "modules/Login/containers/Login";
-import React from "react";
+import React, { Fragment } from "react";
 
 const AppContainer: React.FC = () => {
     const auth = useAuth();
 
-    const isMember = (user?: User) => {
-        if (!user) return false;
-        return user.role === "member";
-    };
-
     return (
-        <>
+        <Fragment>
             <LoadingContainer />
-            {auth.isAuthenticated() && !isMember(auth.user) ? <DashBoardContainer /> : <LoginContainer />}
-        </>
+            {auth.isAuthenticated() && !auth.hasRole("member") ? <DashBoardContainer /> : <LoginContainer />}
+        </Fragment>
     );
 };
 
