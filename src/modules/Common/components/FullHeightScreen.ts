1da70e95ab46841633a9492d17b236681c843859
Chu Phong <chucamphong1999@gmail.com>
2020-03-29 20:02:27 +0700

    Sửa lỗi: 100vh không hoạt động tốt trên các trình duyệt di động

--- src/modules/Common/components/FullHeightScreen.ts
+++ src/modules/Common/components/FullHeightScreen.ts
@@ -2,7 +2,11 @@ import styled from "styled-components";
 
 const FullHeightScreen = styled.div`
     > :first-child {
-        height: 100vh;
+        // noinspection CssInvalidPropertyValue
+        min-height: stretch;
+        
+        // Lỗi: Khi sử dụng trên các trình duyệt chrome hay safari trên điện thoại thì 100vh không còn chạy đúng
+        // min-height: 100vh;
     }
 `;
 
