a9c1b956aab56a178dc09e309b242c958903cce3
Chu Phong <chucamphong1999@gmail.com>
2020-04-30 19:00:17 +0700

    Bổ sung hàm find cho songsService

--- src/services/songsService.ts
+++ src/services/songsService.ts
@@ -1,4 +1,5 @@
 import Query from "@chuphong/query-builder";
+import { Song } from "pages/Song/types";
 import service from "services/service";
 
 const model = "songs";
@@ -6,6 +7,9 @@ const model = "songs";
 export default {
     get(query: string | Query) {
         return service.get(query.toString());
+    },
+    find(songId: number) {
+        return service.get<Song>(`/${model}/${songId}`);
     },
     remove(songId: number) {
         return service.delete(`/${model}/${songId}`);
