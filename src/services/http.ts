12f906feb52b0c03de6f5d5d7491aa1c1e4be35b
Chu Phong <chucamphong1999@gmail.com>
2020-05-04 17:28:25 +0700

    Refactor code

---
+++ src/services/http.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosError, AxiosTransformer } from "axios";
+import api from "config/api";
+
+function transformRequest(requestData: any, headers?: any): any {
+    const plainTextToken = localStorage.getItem("plainTextToken");
+
+    if (plainTextToken) {
+        headers["Authorization"] = `Bearer ${plainTextToken}`;
+    }
+
+    return requestData;
+}
+
+// noinspection JSUnusedLocalSymbols
+function transformResponse(responseData: any, headers?: any): any {
+    if (responseData?.data && !responseData.meta) {
+        return responseData.data;
+    }
+
+    return responseData;
+}
+
+const http = axios.create({
+    baseURL: api.baseURL,
+    responseType: "json",
+    headers: {
+        "X-Requested-With": "XMLHttpRequest",
+    },
+    transformRequest: [transformRequest, ...axios.defaults.transformRequest as AxiosTransformer[]],
+    transformResponse: [transformResponse, ...axios.defaults.transformResponse as AxiosTransformer[]],
+});
+
+http.interceptors.response.use(response => {
+    return response;
+}, (error: AxiosError) => {
+    const statusCode = error.response?.status;
+
+    if (statusCode === 401) {
+        localStorage.removeItem("plainTextToken");
+        window.location.reload();
+    }
+
+    return Promise.reject(error);
+});
+
+export default http;
--- src/services/service.ts
+++ src/services/service.ts
@@ -1,49 +1,6 @@
 import Query from "@chuphong/query-builder";
-import axios, { AxiosError, AxiosInstance, AxiosTransformer } from "axios";
-
-const baseURL = "http://api.cpmusic.com/api";
-
-function transformRequest(requestData: any, headers?: any): any {
-    const plainTextToken = localStorage.getItem("plainTextToken");
-
-    if (plainTextToken) {
-        headers["Authorization"] = `Bearer ${plainTextToken}`;
-    }
-
-    return requestData;
-}
-
-// noinspection JSUnusedLocalSymbols
-function transformResponse(responseData: any, headers?: any): any {
-    if (responseData?.data && !responseData.meta) {
-        return responseData.data;
-    }
-
-    return responseData;
-}
-
-const axiosInstance = axios.create({
-    baseURL,
-    responseType: "json",
-    headers: {
-        "X-Requested-With": "XMLHttpRequest",
-    },
-    transformRequest: [transformRequest, ...axios.defaults.transformRequest as AxiosTransformer[]],
-    transformResponse: [transformResponse, ...axios.defaults.transformResponse as AxiosTransformer[]],
-});
-
-axiosInstance.interceptors.response.use(response => {
-    return response;
-}, (error: AxiosError) => {
-    const statusCode = error.response?.status;
-
-    if (statusCode === 401) {
-        localStorage.removeItem("plainTextToken");
-        window.location.reload();
-    }
-
-    return Promise.reject(error);
-});
+import { AxiosInstance } from "axios";
+import http from "services/http";
 
 export interface APIResponse {
     message: string;
@@ -71,7 +28,7 @@ export interface APIPaginatedResponse<T extends Object> {
 
 export class Service<T extends Object> {
     protected model: string;
-    protected http: AxiosInstance = axiosInstance;
+    protected http: AxiosInstance = http;
 
     constructor(model: string) {
         this.model = model;
@@ -93,16 +50,16 @@ export class Service<T extends Object> {
     }
 
     public create(entity: Partial<T>) {
-        return this.http.post(this.url(), entity);
+        return this.http.post<APIResponse>(this.url(), entity);
     }
 
     public update(entityId: number, data: Partial<T>) {
-        return this.http.patch(this.url(entityId), data);
+        return this.http.patch<APIResponse>(this.url(entityId), data);
     }
 
     public remove(entityId: number) {
-        return this.http.delete(this.url(entityId));
+        return this.http.delete<APIResponse>(this.url(entityId));
     }
 }
 
-export default axiosInstance;
+export default http;
