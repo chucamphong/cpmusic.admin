6f925b6c09e3d4af66d5a23975c43e923e5c8690
Chu Phong <chucamphong1999@gmail.com>
2020-05-08 10:48:14 +0700

    Export các service

---
+++ src/services/index.ts
@@ -0,0 +1,6 @@
+export { default as SongService } from "./songService";
+export { default as ArtistService } from "./artistService";
+export { default as AuthService } from "./authService";
+export { default as CategoryService } from "./categoryService";
+export { default as UserService } from "./userService";
+export type { APIResponse, APIPaginatedResponse } from "./service";
